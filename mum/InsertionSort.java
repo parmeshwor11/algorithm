5726769486289efea3c24d7cd762e6668907915d
pthapa <parmeshwor11@gmail.com>
2016-01-22 16:10:16 -0600

    insertion sort corrected

--- mum/InsertionSort.java
+++ mum/InsertionSort.java
@@ -5,14 +5,47 @@ package algorithm.mum;
  */
 public class InsertionSort {
 
-    static int [] arr = {3,6,4,1,2,5,7,8};
+
 
     public static void main(String[] args) {
 
+        int [] arr = {3,6,4,1,2,5,7,8};
 
+        sort(arr);
 
         for(int k:arr){
             System.out.print(k+" ");
         }
+    }
+
+    private static void sort(int[] arr) {
+
+        // using while loop; here we can observe best case of Insertion sort ie. O(n)
+        for(int i=1;i<arr.length;i++){
+            int temp = i-1;
+           while (temp>=0&&arr[i]<arr[temp]) temp --;
+
+            swap (arr,i,temp+1);
+        }
+
+        // for loop implementation
+
+    /*    for(int i=1;i<arr.length;i++){
+            int min = i;
+            for(int j=i-1;j>=0;j--){
+
+                if(arr[i]<arr[j])
+                    min = j;
+            }
+
+            swap(arr,i,min);
+        }*/
+    }
+
+    private static void swap(int[]a,int i, int j) {
+
+        int temp = a[j];
+        a[j] = a[i];
+        a[i] = temp;
     }
 }
